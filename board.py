fee1391ad317587c7b55a7c3a2570fce5f878505
box16 <inoinobox16@gmail.com>
2024-08-20 18:44:19 +0900

    ファイル移動

--- board.py
+++ board.py
@@ -1,5 +1,25 @@
 from settings import GREEN, BLACK, WHITE
-from piece import PIECE
+
+
+class PIECE:
+    COLOR_SET = {GREEN, BLACK, WHITE}
+
+    def __init__(self):
+        self.color = GREEN
+
+    def set_color(self, color):
+        if color in self.COLOR_SET:
+            self.color = color
+        else:
+            raise Exception("設定可能値以外")
+
+    def turn_color(self):
+        if self.color == GREEN:
+            raise Exception("反転不可")
+        self.color = WHITE if self.color == BLACK else BLACK
+
+    def get_color(self):
+        return self.color
 
 
 class BOARD:
